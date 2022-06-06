8a7b3d2179cb4e5674e7a368e1a8bc6ed0ccc1c9
Bernardngari <bernardngari.ke@gmail.com>
2022-06-06 07:18:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const removeMain = document.querySelector("#main");
+removeMain.remove();
+
+const newHeader = document.createElement( "h1" );
+newHeader.setAttribute( "id", "victory" );
+newHeader.textContent = "YOUR-NAME is the champion";
+
+
+
+
+
